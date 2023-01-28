8ede05a6f5921da558c773f737310ce2315f0c14
Harshita Rawat <harshita.reenu184@gmail.com>
2023-01-28 22:25:24 +0530

    Create even_characters.py

---
+++ even_characters.py
@@ -0,0 +1,10 @@
+# Write a program to accept a string from the user and 
+# display characters that are present at an even index number.
+
+# For example, str = "pynative" so you should display ‘p’, ‘n’, ‘t’, ‘v’.
+string=input('Enter a string: ')
+print(f'Original string is {string} ')
+n=len(string)
+print()
+for i in range(0, n-1, 2):
+    print("index[", i, "]", string[i])
