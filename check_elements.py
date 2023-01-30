cd9b65d5b240110c4775d76fd862dbe5bead0680
Harshita Rawat <harshita.reenu184@gmail.com>
2023-01-30 22:08:34 +0530

    Add files via upload

---
+++ check_elements.py
@@ -0,0 +1,27 @@
+''' Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
+
+Given:
+
+numbers_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
+Expected Output:
+
+Given list: [10, 20, 30, 40, 10]
+result is True
+
+numbers_y = [75, 65, 35, 75, 30]
+result is False '''
+def check_elements():
+    Arr = []
+    n=int(input("Enter the size of Arr: "))
+    print("Enter the elements")
+    for i in range(n):
+        item=int(input())
+        Arr.append(item)
+    for item in Arr:
+        if Arr[0]==Arr[n-1]:
+            print('True')   
+        else:
+            print('False')
+            
+check_elements()
