6af72e693492300076ac1ed074ab101236f9f9b4
Harshita Rawat <harshita.reenu184@gmail.com>
2023-02-18 10:58:54 +0530

    Update duplicates_list_form_a_list_of_int.py

--- duplicates_list_form_a_list_of_int.py
+++ duplicates_list_form_a_list_of_int.py
@@ -10,13 +10,11 @@ Output : output_list = [20, 30, -20, 60]
 Input :  list = [-1, 1, -1, 8]
 Output : output_list = [-1]
 '''
+list = [10, 20, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
 output_list=[]
 i=0
 j=0
 
-list = [10, 20, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
-i=0
-output=[]
 for item in list:
     count=0
     key=list[i]
@@ -29,5 +27,6 @@ for item in list:
             output_list.append(key)
     
     i=i+1
+    
 print("Input: ", list)
 print("Output: ", output_list)
