b5a387099312feb557b60e543af51d7ac493d554
Harshita Rawat <harshita.reenu184@gmail.com>
2023-03-12 23:47:12 +0530

    Add files via upload

---
+++ enumerate.py
@@ -0,0 +1,12 @@
+l1 = ["eat", "sleep", "repeat"]
+s1 = "enum"
+  
+# creating enumerate objects
+obj1 = enumerate(l1)
+obj2 = enumerate(s1)
+  
+print ("Return type:", type(obj1))
+print (list(enumerate(l1)))
+  
+# changing start index to 2 from 0
+print (list(enumerate(s1, 2)))
